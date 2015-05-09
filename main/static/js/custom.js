105e4869927f8d1ae6a6d8832fceed8577fe7dca
Tae Hwan Kim <taedori@outlook.com>
2015-05-09 15:11:45 -0700

    Set up using bower

---
+++ main/static/js/custom.js
@@ -0,0 +1,16 @@
+//$(function(){
+//    $('#map').gMap({
+//        address: "",
+//	    zoom: 18,
+//	    markers:[
+//		{
+//			latitude: 34.0492440,
+//			longitude: -118.2735170,
+//			html: "Tyndale International University",
+//            popup: true,
+//		},
+//
+//	]
+//    });
+//});
+//
